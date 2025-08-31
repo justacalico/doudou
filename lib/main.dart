40f1716fbc876033549ed57e105fd66ce4b08d57
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-30 20:38:23 -0700

    Refactor navigation: replace bottomBar with navbar and update imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'providers/app_state.dart';
 import 'screens/login/login.dart';
-import 'screens/controller/bottomBar.dart';
+import 'screens/controller/navbar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
