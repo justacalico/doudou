52aba2a6fc0663f7e4f68e8adcca41538361f096
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-30 21:28:58 -0700

    Wrap CupertinoApp with AudioServiceWidget for enhanced audio management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:audio_service/audio_service.dart';
 import 'providers/app_state.dart';
 import 'screens/Login/login.dart';
 import 'screens/controller/navbar.dart';
@@ -17,22 +18,24 @@ class DoudouApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => AppState(),
-      child: CupertinoApp(
-        title: 'Doudou - Jellyfin Music Player',
-        theme: const CupertinoThemeData(
-          primaryColor: CupertinoColors.systemPurple,
-          scaffoldBackgroundColor: CupertinoColors.systemBackground,
+      child: AudioServiceWidget(
+        child: CupertinoApp(
+          title: 'Doudou - Jellyfin Music Player',
+          theme: const CupertinoThemeData(
+            primaryColor: CupertinoColors.systemPurple,
+            scaffoldBackgroundColor: CupertinoColors.systemBackground,
+          ),
+          home: Consumer<AppState>(
+            builder: (context, appState, child) {
+              if (appState.isLoggedIn) {
+                return const HomeScreen();
+              } else {
+                return const LoginScreen();
+              }
+            },
+          ),
+          debugShowCheckedModeBanner: false,
         ),
-        home: Consumer<AppState>(
-          builder: (context, appState, child) {
-            if (appState.isLoggedIn) {
-              return const HomeScreen();
-            } else {
-              return const LoginScreen();
-            }
-          },
-        ),
-        debugShowCheckedModeBanner: false,
       ),
     );
   }
