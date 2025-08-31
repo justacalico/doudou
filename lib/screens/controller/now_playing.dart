7ae96c8295caa465d90700d57a3a13cdb3a75173
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-31 09:12:32 -0700

    Replace LyricsOverlay with SyncedLyricsOverlay in NowPlayingScreen

--- lib/screens/controller/now_playing.dart
+++ lib/screens/controller/now_playing.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:just_audio/just_audio.dart';
 import '../../providers/app_state.dart';
 import '../../widgets/music_visualizer.dart';
-import '../../widgets/lyrics_overlay.dart';
+import '../../widgets/synced_lyrics_overlay.dart';
 import '../queue/queue.dart';
 
 class NowPlayingScreen extends StatefulWidget {
