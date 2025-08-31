35127c18ac83e1d9e14750e4080cd8812b115bbc
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-30 20:00:09 -0700

    Add Material package import to NowPlayingScreen for enhanced widget support

--- lib/screens/now_playing_screen.dart
+++ lib/screens/now_playing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:just_audio/just_audio.dart';
 import '../providers/app_state.dart';
