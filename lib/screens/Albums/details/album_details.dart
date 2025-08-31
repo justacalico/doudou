f50d3d72485f513d1256d1803614718597eb7cc7
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-30 20:56:38 -0700

    Fix positioning of MiniPlayer widget in AlbumDetailScreen

--- lib/screens/Albums/details/album_details.dart
+++ lib/screens/Albums/details/album_details.dart
@@ -242,6 +242,13 @@ class _AlbumDetailScreenState extends State<AlbumDetailScreen> {
             ),
         ],
       ),
+      const Positioned(
+        left: 0,
+        right: 0,
+        bottom: 0,
+        child: MiniPlayer(),
+      ),
+    ],
     );
   }
 
