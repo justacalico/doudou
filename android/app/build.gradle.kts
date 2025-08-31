e5030fc1466fcdfca1df8b93af6a0b7b9e94d376
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-30 22:24:43 -0700

    Update application namespace to match project structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.doudou"
+    namespace = "gitlab.httpanimations.doudou"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
