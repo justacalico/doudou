4f8ef272d35d97b3f225c3a2cdf4d5bb7e236201
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-30 20:57:10 -0700

    Add missing import for mini_player widget in albums.dart

--- lib/screens/Albums/albums.dart
+++ lib/screens/Albums/albums.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../../providers/app_state.dart';
 import '../../models/jellyfin_models.dart';
+import '../../widgets/mini_player.dart';
 import 'details/album_details.dart';
 
 class AlbumsTab extends StatelessWidget {
