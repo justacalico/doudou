5e0136d6122c5d908d9c51c555fdb51c00cbf6f1
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-31 09:49:52 -0700

    Remove unused import for CachedNetworkImage in albums.dart

--- lib/screens/albums/albums.dart
+++ lib/screens/albums/albums.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import '../../providers/app_state.dart';
 import '../../models/jellyfin_models.dart';
 import '../../widgets/mini_player.dart';
