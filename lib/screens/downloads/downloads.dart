3daa8fc19c3f94558df156206b47610c5712cc14
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-01 12:04:02 -0700

    feat: Increase tab count in DownloadsScreen from 2 to 3

--- lib/screens/downloads/downloads.dart
+++ lib/screens/downloads/downloads.dart
@@ -24,7 +24,7 @@ class _DownloadsScreenState extends State<DownloadsScreen>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 2, vsync: this);
+    _tabController = TabController(length: 3, vsync: this);
   }
 
   @override
