73b0664b168be3020792e7a6c80a9a091889ef0c
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-31 21:49:14 -0700

    refactor: Remove unused _formatDuration method from ArtistDetailScreen

--- lib/screens/artists/details/artist_detail.dart
+++ lib/screens/artists/details/artist_detail.dart
@@ -563,10 +563,4 @@ class _ArtistDetailScreenState extends State<ArtistDetailScreen> {
     );
   }
 
-  String _formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = duration.inMinutes.remainder(60);
-    final seconds = duration.inSeconds.remainder(60);
-    return '${twoDigits(minutes)}:${twoDigits(seconds)}';
-  }
 }
--- lib/screens/partials/tracks/track_list_item.dart
+++ lib/screens/partials/tracks/track_list_item.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
