8a4b17027ff2d866b6a86e6bdaf662bb04ce2d54
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-31 21:07:29 -0700

    refactor: Remove unused import of 'dart:ui' in visualizer.dart

--- lib/screens/playing/visualizer/visualizer.dart
+++ lib/screens/playing/visualizer/visualizer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'dart:math';
 import 'dart:async';
-import 'dart:ui' as ui;
 
 // Embedded visualizer widget for the now playing screen
 class EmbeddedVisualizer extends StatefulWidget {
