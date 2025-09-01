a24a53c577e5779acd96ca73d835218faecbb06b
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-31 18:42:39 -0700

    Fix: add missing import for foundation package in playlists.dart

--- lib/screens/playlists/playlists.dart
+++ lib/screens/playlists/playlists.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../../providers/app_state.dart';
