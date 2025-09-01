80c29a0a4aa6855f535b6db447413b236272e03e
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-01 12:54:56 -0700

    fix: Set initial index for TabController in DownloadsScreen

--- lib/screens/downloads/downloads.dart
+++ lib/screens/downloads/downloads.dart
@@ -27,7 +27,7 @@ class _DownloadsScreenState extends State<DownloadsScreen>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 4, vsync: this);
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
   }
 
   @override
