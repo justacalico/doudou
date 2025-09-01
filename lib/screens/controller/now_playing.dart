29a441e51a953424b07596d22a2f8c13fe81a90e
HttpAnimations <shawndouglasyt@gmail.com>
2025-08-31 20:49:09 -0700

    refactor: Replace synced lyrics overlay with new lyrics overlay implementation and update import paths

--- lib/screens/controller/now_playing.dart
+++ lib/screens/controller/now_playing.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:audio_service/audio_service.dart';
 import '../../providers/app_state.dart';
-import '../playing/lyrics/synced_lyrics_overlay.dart';
+import '../playing/lyrics/lyrics_overlay.dart';
 import '../playing/queue/queue_overlay.dart';
 import '../../widgets/cached_image_widget.dart';
 import '../playing/visualizer/visualizer.dart';
