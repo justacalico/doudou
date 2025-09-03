0b94e5fa26972c152a67a95acdfe0bf7289138dc
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-03 14:59:15 -0700

    feat: Add import for queue functionality in DownloadsScreen

--- lib/screens/downloads/downloads.dart
+++ lib/screens/downloads/downloads.dart
@@ -9,6 +9,7 @@ import '../../services/download_service.dart';
 import 'playlists/playlists.dart';
 import 'albums/albums.dart';
 import 'songs/songs.dart';
+import 'queue/queue.dart';
 
 class DownloadsScreen extends StatefulWidget {
   const DownloadsScreen({super.key});
