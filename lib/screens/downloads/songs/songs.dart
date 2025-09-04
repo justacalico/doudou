ba13746faa0e8a0cfc56bbabf5b6e1f59a6fa8d7
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-04 12:58:19 -0700

    fix: Add missing import for Flutter widgets in songs.dart

--- lib/screens/downloads/songs/songs.dart
+++ lib/screens/downloads/songs/songs.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../../../providers/app_state.dart';
