aa122748a6a88940ad084908dfa70e7d96392ed8
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-04 12:59:18 -0700

    fix: Remove unused import for Flutter widgets in songs.dart

--- lib/screens/downloads/songs/songs.dart
+++ lib/screens/downloads/songs/songs.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../../../providers/app_state.dart';
