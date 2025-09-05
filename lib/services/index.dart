367bff945a309b2e5bcfed8c5e923c313e2e763e
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-04 19:24:43 -0700

    feat: Add barrel export for all services

---
+++ lib/services/index.dart
@@ -0,0 +1,7 @@
+// Barrel export for all services
+export 'audio/index.dart';
+export 'cache_service.dart';
+export 'download_service.dart';
+export 'image_cache_manager.dart';
+export 'jellyfin_service.dart';
+export 'lyrics_service.dart';
