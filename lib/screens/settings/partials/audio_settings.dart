46e56cdbb4b02a258f21c5e2d38b4aefe249b6a6
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-04 19:58:11 -0700

    feat: Change background color from pure black to dark gray in audio settings section

--- lib/screens/settings/partials/audio_settings.dart
+++ lib/screens/settings/partials/audio_settings.dart
@@ -13,7 +13,7 @@ class AudioSettingsSection extends StatelessWidget {
         return Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           decoration: BoxDecoration(
-            color: const Color(0xFF000000), // Pure black background
+            color: const Color(0xFF1C1C1E), // Dark gray background instead of pure black
             borderRadius: BorderRadius.circular(16),
             border: Border.all(
               color: const Color(0xFF2C2C2E),
