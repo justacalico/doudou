f7635800328cbf617e079427c9309a8d480327c9
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-04 19:27:40 -0700

    feat: Create main barrel export for the application

---
+++ lib/index.dart
@@ -0,0 +1,6 @@
+// Main barrel export for the entire application
+export 'models/index.dart';
+export 'providers/index.dart';
+export 'screens/index.dart';
+export 'services/index.dart';
+export 'widgets/index.dart';
