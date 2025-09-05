96d6373ab65d1a93f0b861aa4c25de1f471779a8
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-04 19:27:13 -0700

    feat: Add barrel export for all screens

---
+++ lib/screens/index.dart
@@ -0,0 +1,15 @@
+// Barrel export for all screens
+export 'albums/index.dart';
+export 'artists/index.dart';
+export 'controller/index.dart';
+export 'downloads/index.dart';
+export 'favorites/index.dart';
+export 'home/index.dart';
+export 'libary/index.dart';
+export 'login/index.dart';
+export 'partials/index.dart';
+export 'playing/index.dart';
+export 'playlists/index.dart';
+export 'search/index.dart';
+export 'settings/index.dart';
+export 'songs/index.dart';
