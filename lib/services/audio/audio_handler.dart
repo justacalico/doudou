3f59614c0f3d412958074d859886856008003299
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-06 10:50:58 -0700

    fix: Remove Android audio attributes configuration from AudioPlayer initialization

--- lib/services/audio/audio_handler.dart
+++ lib/services/audio/audio_handler.dart
@@ -10,15 +10,7 @@ import '../jellyfin_service.dart';
 import '../download_service.dart';
 
 class DoudouAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
-  final AudioPlayer _player = AudioPlayer(
-    audioPipeline: AudioPipeline(
-      androidAudioAttributes: const AndroidAudioAttributes(
-        contentType: AndroidAudioContentType.music,
-        flags: AndroidAudioFlags.audibilityEnforced,
-        usage: AndroidAudioUsage.media,
-      ),
-    ),
-  );
+  final AudioPlayer _player = AudioPlayer();
   final JellyfinService _jellyfinService;
   final DownloadService _downloadService;
   
