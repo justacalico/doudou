ae50a4b70a0d66be162ac85f7df95d81eff3c2fc
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-07 21:35:13 -0700

    refactor: Remove _stopCompletionChecker call in dispose method to streamline resource cleanup

--- lib/services/audio/audio_handler.dart
+++ lib/services/audio/audio_handler.dart
@@ -1555,7 +1555,6 @@ class DoudouAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler
 
   void dispose() {
     _stopPeriodicSaving();
-    _stopCompletionChecker();
     _savePlaybackState();
     _clearPreloadedPlayers();
     _player.dispose();
