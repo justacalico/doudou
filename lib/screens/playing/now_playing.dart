e66288e52fe684648296a7f4059b6dda9dd8816c
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-08 17:51:02 -0700

    fix: Import 'dart:math' for mathematical operations in NowPlayingScreen

--- lib/screens/playing/now_playing.dart
+++ lib/screens/playing/now_playing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
+import 'dart:math' as math;
 import 'package:provider/provider.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:audio_service/audio_service.dart';
