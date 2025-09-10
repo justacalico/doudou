b1f00e8a79527062a9d280b359dea9f9148c0a6d
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-09 19:48:28 -0700

    fix: Import Material package for consistent UI components

--- lib/screens/partials/navbar/navbar.dart
+++ lib/screens/partials/navbar/navbar.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../providers/app_state.dart';
 import '../../home/home.dart';
