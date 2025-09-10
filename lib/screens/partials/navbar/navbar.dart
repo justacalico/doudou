0abea24206c7ef3590845e40f5e5464c93d3607b
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-09 19:29:45 -0700

    fix: Improve tab bar background color opacity for better visibility

--- lib/screens/partials/navbar/navbar.dart
+++ lib/screens/partials/navbar/navbar.dart
@@ -104,12 +104,12 @@ class _HomeScreenState extends State<HomeScreen> {
             CupertinoTabScaffold(
               controller: _tabController,
               tabBar: CupertinoTabBar(
-                backgroundColor: CupertinoColors.black.withOpacity(0.8),
+                backgroundColor: const Color(0xFF000000).withOpacity(0.95),
                 activeColor: CupertinoColors.systemRed, // Red for active tab
                 inactiveColor: CupertinoColors.systemGrey2,
                 border: Border(
                   top: BorderSide(
-                    color: CupertinoColors.white.withOpacity(0.1),
+                    color: CupertinoColors.white.withOpacity(0.2),
                     width: 0.5,
                   ),
                 ),
