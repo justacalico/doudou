13066f464788585ad79b95900f5df8b184651f0f
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-09 18:59:51 -0700

    fix: Adjust border radius for button in HomeContent widget

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -209,7 +209,7 @@ class _HomeContentState extends State<HomeContent> {
                             height: 50,
                             decoration: BoxDecoration(
                               color: const Color(0xFFE91E63),
-                              borderRadius: BorderRadius.circular(25),
+                              borderRadius: BorderRadius.circular(12),
                             ),
                             child: const Row(
                               mainAxisAlignment: MainAxisAlignment.center,
