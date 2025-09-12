f2a537bf5755dba204966ee1f420f8ffaa500a40
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-11 22:36:12 -0700

    feat: add mediaStateStream to report combined media item and playback state

---
+++ lib/services/audio/media_state_stream.dart
@@ -0,0 +1,18 @@
+import 'package:audio_service/audio_service.dart';
+import 'package:rxdart/rxdart.dart';
+
+class MediaState {
+  final MediaItem? mediaItem;
+  final PlaybackState playbackState;
+
+  MediaState(this.mediaItem, this.playbackState);
+}
+
+/// A stream reporting the combined state of the current media item and its
+/// current position.
+Stream<MediaState> mediaStateStream(BaseAudioHandler audioHandler) {
+  return Rx.combineLatest2<MediaItem?, PlaybackState, MediaState>(
+      audioHandler.mediaItem,
+      audioHandler.playbackState,
+      (mediaItem, playbackState) => MediaState(mediaItem, playbackState));
+}
