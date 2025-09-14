e1a8c28be90d92168b64e4205dbf4283db94b4a7
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-14 00:57:11 -0700

    fix: Add missing audio_service import to navbar.dart

--- lib/screens/partials/navbar/navbar.dart
+++ lib/screens/partials/navbar/navbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
+import 'package:audio_service/audio_service.dart';
 import 'dart:ui';
 import '../../../providers/app_state.dart';
 import '../../../models/jellyfin_models.dart';
