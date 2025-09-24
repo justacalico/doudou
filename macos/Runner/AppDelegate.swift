811ace5b666607995471034251b0e6c8489c36b0
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-23 22:25:27 -0700

    feat: remove Touch Bar plugin registration from AppDelegate

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,15 +3,6 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationDidFinishLaunching(_ notification: Notification) {
-    super.applicationDidFinishLaunching(notification)
-    
-    // Register Touch Bar plugin
-    if let flutterViewController = mainFlutterWindow?.contentViewController as? FlutterViewController {
-      TouchBarPlugin.register(with: flutterViewController.registrar(forPlugin: "TouchBarPlugin"))
-    }
-  }
-
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
