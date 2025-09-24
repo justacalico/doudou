181285672fb777ba89e359f3f4b7ead0b8a23a5a
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-23 22:53:06 -0700

    feat: remove unused lastLyricsUpdate field from DoudouAudioHandler

--- lib/services/audio/audio_handler.dart
+++ lib/services/audio/audio_handler.dart
@@ -28,7 +28,6 @@ class DoudouAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler
   LyricsResult? _currentLyrics;
   int _currentLyricsLineIndex = -1;
   String? _lastLyricsTrackId;
-  DateTime? _lastLyricsUpdate;
   
   // Component managers
   late final AudioStateManager _stateManager;
