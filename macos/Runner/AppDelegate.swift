8a919b00d9f2c6cda2fb6e62512ff49b2de4103c
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-23 22:12:43 -0700

    feat: register Touch Bar plugin in AppDelegate for macOS

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,6 +3,15 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    super.applicationDidFinishLaunching(notification)
+    
+    // Register Touch Bar plugin
+    if let flutterViewController = mainFlutterWindow?.contentViewController as? FlutterViewController {
+      TouchBarPlugin.register(with: flutterViewController.registrar(forPlugin: "TouchBarPlugin"))
+    }
+  }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
