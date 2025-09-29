139bcfea77a8b489ff624fe41c18c4c93d84db23
HttpAnimations <shawndouglasyt@gmail.com>
2025-09-29 12:21:25 -0700

    fix: remove unused platform detection for web in app_state.dart

--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -22,7 +22,6 @@ class AppState extends ChangeNotifier {
   bool get _isIOS => !kIsWeb && defaultTargetPlatform == TargetPlatform.iOS;
   bool get _isMacOS => !kIsWeb && defaultTargetPlatform == TargetPlatform.macOS;
   bool get _isLinux => !kIsWeb && defaultTargetPlatform == TargetPlatform.linux;
-  bool get _isWeb => kIsWeb;
   
   bool _isLoggedIn = false;
   bool _isLoading = false;
