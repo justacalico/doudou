ef268572741545aa95efc633341a2f8f67d7005a
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-05 13:47:16 -0700

    fix: add missing import for home page in main.dart

--- lib/desktop/main.dart
+++ lib/desktop/main.dart
@@ -7,6 +7,7 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:just_audio_media_kit/just_audio_media_kit.dart';
 import '../providers/app_state.dart';
 import '../screens/login/login.dart';
+import 'pages/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
