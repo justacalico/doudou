232eda501d933535ad85d28fdf5ba36c2faf6e58
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-12 22:43:05 -0700

    Refactor _getImageUrl method to use AppState's getImageUrl for image retrieval

--- lib/desktop/pages/details/artist_details.dart
+++ lib/desktop/pages/details/artist_details.dart
@@ -64,7 +64,7 @@ class _ArtistDetailsPageState extends State<ArtistDetailsPage> {
 
   String? _getImageUrl(AppState appState, String? imageId) {
     if (imageId == null) return null;
-    return appState.jellyfinService.getImageUrl(imageId);
+    return appState.getImageUrl(imageId);
   }
 
   @override
