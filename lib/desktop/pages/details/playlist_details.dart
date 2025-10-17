42c2c2579831a73b8257208c9fcdab15be139664
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-16 21:46:22 -0700

    Remove _formatDuration method from _PlaylistDetailsPageState to simplify duration handling

--- lib/desktop/pages/details/playlist_details.dart
+++ lib/desktop/pages/details/playlist_details.dart
@@ -298,12 +298,7 @@ class _PlaylistDetailsPageState extends State<PlaylistDetailsPage> {
     }
   }
 
-  String _formatDuration(int milliseconds) {
-    final duration = Duration(milliseconds: milliseconds);
-    final minutes = duration.inMinutes;
-    final seconds = duration.inSeconds % 60;
-    return '$minutes:${seconds.toString().padLeft(2, '0')}';
-  }
+
 
   void _showEditPlaylistDialog() {
     showDialog(
