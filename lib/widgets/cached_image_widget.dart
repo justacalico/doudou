b2651250a32a33c42e36d5b1d0cdec6a5237611a
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-16 22:31:23 -0700

    Remove debug print statements from AlbumArtWidget for cleaner output

--- lib/widgets/cached_image_widget.dart
+++ lib/widgets/cached_image_widget.dart
@@ -85,16 +85,7 @@ class AlbumArtWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (kDebugMode && imageUrl != null) {
-      if (kDebugMode) {
-        print('AlbumArtWidget: imageUrl = $imageUrl');
-      }
-    }
-    
     if (imageUrl == null) {
-      if (kDebugMode) {
-        print('AlbumArtWidget: imageUrl is null, showing placeholder');
-      }
       return Container(
         width: size,
         height: size,
