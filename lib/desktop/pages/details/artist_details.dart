4907591f0a2e833960dac6795d7a1eb46977089f
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-16 21:46:31 -0700

    Add missing import for TrackListTemplate in artist_details.dart

--- lib/desktop/pages/details/artist_details.dart
+++ lib/desktop/pages/details/artist_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../templates/desktop_layout.dart';
+import '../../templates/track_list_template.dart';
 import '../../../providers/app_state.dart';
 import '../../../models/jellyfin_models.dart';
 import 'album_details.dart';
