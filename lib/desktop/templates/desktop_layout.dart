d4a6a6767e020d979a6b5db3e6ca46682f6b4c8e
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-16 17:50:11 -0700

    Add import for foundation package in desktop_layout.dart

--- lib/desktop/templates/desktop_layout.dart
+++ lib/desktop/templates/desktop_layout.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:audio_service/audio_service.dart';
