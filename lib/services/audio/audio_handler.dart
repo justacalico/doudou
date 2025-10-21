1766b8ef7d0487390f7bc960b43be0af30defc21
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-20 20:40:04 -0700

    Add missing import for Android service manager in audio handler

--- lib/services/audio/audio_handler.dart
+++ lib/services/audio/audio_handler.dart
@@ -21,6 +21,7 @@ import 'async_mutex.dart';
 import 'audio_operation_queue.dart';
 import 'audio_state_machine.dart';
 import 'operation_cancellation.dart';
+import 'android_service_manager.dart';
 
 class DoudouAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
   final AudioPlayer _player = AudioPlayer();
