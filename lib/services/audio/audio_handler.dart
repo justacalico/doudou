6e3f34fef0589306ee658ca3ae21f4c3afedb1dd
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-22 04:15:17 -0700

    Add missing import for Flutter material package in audio_handler.dart

--- lib/services/audio/audio_handler.dart
+++ lib/services/audio/audio_handler.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:audio_service/audio_service.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import '../../models/jellyfin_models.dart';
 import '../jellyfin_service.dart';
 import '../media_service_manager.dart';
