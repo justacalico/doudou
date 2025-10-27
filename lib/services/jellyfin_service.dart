28c41e8d9747ae771aba6c16391ba4da6643190f
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-27 00:52:47 -0700

    Improve debug logging for image URL caching in getImageUrl method

--- lib/services/jellyfin_service.dart
+++ lib/services/jellyfin_service.dart
@@ -550,7 +550,9 @@ class JellyfinService implements BaseMediaService {
     
     // Only log first generation of each URL, not repeated calls
     if (kDebugMode && _imageUrlCache.length % 50 == 1) {
-      print('JellyfinService.getImageUrl: Cached ${_imageUrlCache.length} image URLs');
+      if (kDebugMode) {
+        print('JellyfinService.getImageUrl: Cached ${_imageUrlCache.length} image URLs');
+      }
     }
     
     return imageUrl;
