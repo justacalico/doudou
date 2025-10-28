6e6e6ad5e7679d8771a6c334bef78910cbe52d10
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-27 18:06:55 -0700

    Add slide transition for opening Now Playing screen in DynamicIsle widget

--- lib/widgets/isle.dart
+++ lib/widgets/isle.dart
@@ -52,6 +52,31 @@ class _DynamicIsleState extends State<DynamicIsle>
     });
   }
 
+  void _openNowPlaying(BuildContext context) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => const NowPlayingScreen(),
+        transitionDuration: const Duration(milliseconds: 300),
+        reverseTransitionDuration: const Duration(milliseconds: 300),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(0.0, 1.0); // Start from bottom
+          const end = Offset.zero; // End at current position
+          const curve = Curves.easeInOut;
+
+          var tween = Tween(begin: begin, end: end).chain(
+            CurveTween(curve: curve),
+          );
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppState>(
