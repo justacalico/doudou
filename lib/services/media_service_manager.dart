edbb6fe6bc6db26b702b596a6a547bbdc3353d67
HttpAnimations <shawndouglasyt@gmail.com>
2025-10-29 00:38:43 -0700

    Add support for improved stream URL retrieval in PlexService

--- lib/services/media_service_manager.dart
+++ lib/services/media_service_manager.dart
@@ -167,6 +167,13 @@ class MediaServiceManager {
       return jellyfinAdapter.getDirectStreamUrl(trackId);
     }
     
+    if (_currentService is PlexService) {
+      final plexService = _currentService as PlexService;
+      // For Plex, try to get the best available stream URL
+      // Since this is synchronous, we can't fetch part ID, so use improved universal transcode
+      return plexService.getImprovedUniversalStreamUrl(trackId);
+    }
+    
     // For other services, fallback to regular stream URL
     return _currentService!.getStreamUrl(trackId);
   }
