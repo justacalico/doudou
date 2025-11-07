8f58c269355db9b0499b007ea0a7b1edd853672b
HttpAnimations <shawndouglasyt@gmail.com>
2025-11-06 21:51:29 -0700

    Remove flutter_wear_os_connectivity namespace fix from build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,17 +5,6 @@ allprojects {
     }
 }
 
-// Fix for flutter_wear_os_connectivity namespace issue
-subprojects {
-    afterEvaluate {
-        if (project.name == "flutter_wear_os_connectivity") {
-            android {
-                namespace = "github.ssttonn.flutter_wear_os_connectivity"
-            }
-        }
-    }
-}
-
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
