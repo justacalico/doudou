a41bae7c83728d2fec83472e96cfec5fa91d14db
HttpAnimations <shawndouglasyt@gmail.com>
2025-11-06 21:46:50 -0700

    Add import for WearOS service in AppState

--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -15,6 +15,7 @@ import '../services/cache_service.dart';
 import '../services/image_cache_manager.dart';
 import '../services/download_service.dart';
 import '../services/logging_service.dart';
+import '../services/wearos_service.dart';
 
 class AppState extends ChangeNotifier {
   final JellyfinService _jellyfinService = JellyfinService();
