a92e0e8cd832f93ff55b060e441ca1b2e99dcebc
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-09 01:42:22 -0800

    Add localization support import to desktop layout

--- lib/desktop/templates/desktop_layout.dart
+++ lib/desktop/templates/desktop_layout.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:audio_service/audio_service.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../pages/home.dart';
 import '../services/navigation_service.dart';
 import '../../providers/app_state.dart';
