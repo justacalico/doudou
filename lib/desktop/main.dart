e9be10860ce7ee98f2ee318bf5e16660234f6859
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-09 01:29:56 -0800

    Add localization support to DesktopDoudouApp with delegates and locale settings

--- lib/desktop/main.dart
+++ lib/desktop/main.dart
@@ -6,6 +6,8 @@ import 'package:provider/provider.dart';
 import 'package:audio_service/audio_service.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:just_audio_media_kit/just_audio_media_kit.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import '../l10n/generated/app_localizations.dart';
 import '../providers/app_state.dart';
 import '../services/logging_service.dart';
 import '../screens/login/login.dart';
@@ -201,6 +203,14 @@ class DesktopDoudouApp extends StatelessWidget {
                 ),
               ),
               themeMode: appState.themeMode,
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+              ],
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: appState.locale,
               home: Consumer<AppState>(
                 builder: (context, appState, child) {
                   // Show loading screen while initializing
