677dc4dfe72b2d8e8e70eea08c81d4f108c3b09c
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-10 11:13:16 -0700

    Add missing imports for UI and Apple theme in LoginScreen

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -4,7 +4,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:vibration/vibration.dart';
+import 'dart:ui';
 import '../../providers/app_state.dart';
+import '../../widgets/apple_design/apple_theme.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
