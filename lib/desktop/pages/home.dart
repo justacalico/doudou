44f979cbb3965de8f89a305f960496eac31cdff8
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-10 11:14:28 -0700

    Add missing imports for UI and Apple theme in HomePage

--- lib/desktop/pages/home.dart
+++ lib/desktop/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import 'package:provider/provider.dart';
 import '../../l10n/app_localizations.dart';
+import '../../widgets/apple_design/apple_theme.dart';
 import '../templates/page_template.dart';
 import '../templates/music_cards.dart';
 import '../../providers/app_state.dart';
