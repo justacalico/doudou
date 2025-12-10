50da0ace16ec374bfd3a478462152ed75f745583
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-10 15:39:05 -0700

    Reset expanded sections state on search clear

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -134,6 +134,10 @@ class _SearchScreenState extends State<SearchScreen> {
         _playlistResults = [];
         _isSearching = false;
         _searchQuery = '';
+        // Reset expanded states
+        _artistsExpanded = false;
+        _albumsExpanded = false;
+        _tracksExpanded = false;
       });
       return;
     }
