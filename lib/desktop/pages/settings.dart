c9113bd4ab4b0cabbd98523fc3e4aa4e52c860a7
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-10 11:15:23 -0700

    Add missing imports for UI and Apple theme in SettingsPage

--- lib/desktop/pages/settings.dart
+++ lib/desktop/pages/settings.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'dart:io';
+import 'dart:ui';
 import 'package:package_info_plus/package_info_plus.dart';
+import '../../widgets/apple_design/apple_theme.dart';
 import '../templates/page_template.dart';
 import '../../providers/app_state.dart';
 import '../../services/logging_service.dart';
