cc5e331816470a6d1b4c80c7f46e98117a696d38
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-10 11:18:07 -0700

    Remove unused imports for dart:ui and Apple theme in AlbumsPage

--- lib/desktop/pages/albums.dart
+++ lib/desktop/pages/albums.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
 import 'package:provider/provider.dart';
 import '../../l10n/app_localizations.dart';
-import '../../widgets/apple_design/apple_theme.dart';
 import '../templates/page_template.dart';
 import '../../providers/app_state.dart';
 import '../../models/jellyfin_models.dart';
