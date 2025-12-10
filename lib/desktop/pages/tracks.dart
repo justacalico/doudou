6be785597d39769055dfb88ef4e342ba2812f8f9
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-10 11:17:36 -0700

    Add missing imports for dart:ui and Apple theme in TracksPage

--- lib/desktop/pages/tracks.dart
+++ lib/desktop/pages/tracks.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import 'package:provider/provider.dart';
 import '../../l10n/app_localizations.dart';
+import '../../widgets/apple_design/apple_theme.dart';
 import '../templates/page_template.dart';
 import '../templates/desktop_layout.dart';
 import 'details/media_details.dart';
