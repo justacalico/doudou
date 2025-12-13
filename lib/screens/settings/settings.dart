7e011f403533d5817264138d4a6141bfc116f3d9
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-12 18:46:08 -0700

    Add missing import for url_launcher in settings.dart

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../providers/app_state.dart';
 import '../../l10n/app_localizations.dart';
 import '../../widgets/apple_design/liquid_glass.dart';
