2612c2477dc0039b71e9411a375802a426dece89
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-18 15:18:19 -0700

    fix: Replace Navidrome with Subsonic in ServerType enum

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -1,6 +1,6 @@
 import '../models/jellyfin_models.dart';
 
-enum ServerType { jellyfin, plex, navidrome, swingmusic, local }
+enum ServerType { jellyfin, plex, subsonic, swingmusic, local }
 
 /// Base interface for all media server services
 abstract class BaseMediaService {
