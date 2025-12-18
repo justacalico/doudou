dd33ac698b96e1ccdf87380e8a13b67f89e56c15
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-18 01:24:15 -0700

    refactor: Comment out Swing Music API integration until functionality is restored

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -745,15 +745,16 @@ class _LoginScreenState extends State<LoginScreen>
                   AppleColors.systemBlue,
                   isDark,
                 ),
-                const SizedBox(width: 12),
-                _buildServerTypeCard(
-                  'swingmusic',
-                  'Swing',
-                  null,
-                  AppleColors.systemTeal,
-                  isDark,
-                  icon: CupertinoIcons.music_note_2,
-                ),
+                // TODO: Re-enable when Swing Music API is working
+                // const SizedBox(width: 12),
+                // _buildServerTypeCard(
+                //   'swingmusic',
+                //   'Swing',
+                //   null,
+                //   AppleColors.systemTeal,
+                //   isDark,
+                //   icon: CupertinoIcons.music_note_2,
+                // ),
                 const SizedBox(width: 12),
                 _buildServerTypeCard(
                   'local',
@@ -794,15 +795,16 @@ class _LoginScreenState extends State<LoginScreen>
                   AppleColors.systemBlue,
                   isDark,
                 ),
-                const SizedBox(width: 10),
-                _buildServerTypeChip(
-                  'swingmusic',
-                  'Swing',
-                  null,
-                  AppleColors.systemTeal,
-                  isDark,
-                  icon: CupertinoIcons.music_note_2,
-                ),
+                // TODO: Re-enable when Swing Music API is working
+                // const SizedBox(width: 10),
+                // _buildServerTypeChip(
+                //   'swingmusic',
+                //   'Swing',
+                //   null,
+                //   AppleColors.systemTeal,
+                //   isDark,
+                //   icon: CupertinoIcons.music_note_2,
+                // ),
                 const SizedBox(width: 10),
                 _buildServerTypeChip(
                   'local',
