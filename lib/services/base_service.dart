7562848b6cadd9823c6db92c6b314826df0c8c18
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-17 17:39:17 -0700

    fix: Add 'local' to ServerType enum for expanded server support

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -1,6 +1,6 @@
 import '../models/jellyfin_models.dart';
 
-enum ServerType { jellyfin, plex, navidrome }
+enum ServerType { jellyfin, plex, navidrome, local }
 
 /// Base interface for all media server services
 abstract class BaseMediaService {
