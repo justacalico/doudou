a8c42b16f2e1f3ed80ba7eb6dc07d92affc4c657
HttpAnimations <shawndouglasyt@gmail.com>
2026-05-09 14:49:29 -0700

    Fixed linux builds

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -18,42 +18,6 @@ struct _MyApplication {
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
-// Implements GApplication::activate.
-static void my_application_activate(GApplication* application) {
-  MyApplication* self = MY_APPLICATION(application);
-  GtkWindow* window =
-      GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
-
-  gtk_window_set_title(window, "doudou");
-  gtk_window_set_decorated(window, FALSE);
-  gtk_window_set_default_size(window, 1280, 720);
-  gtk_widget_show(GTK_WIDGET(window));
-
-  g_autoptr(FlDartProject) project = fl_dart_project_new();
-  fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
-
-  FlView* view = fl_view_new(project);
-  gtk_widget_show(GTK_WIDGET(view));
-  gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
-
-  fl_register_plugins(FL_PLUGIN_REGISTRY(view));
-
-  gtk_widget_grab_focus(GTK_WIDGET(view));
-
-  setup_window_control_channel(view);
-}
-
-static void setup_window_control_channel(FlView* view) {
-  FlEngine* engine = fl_view_get_engine(view);
-  g_autoptr(FlMethodChannel) channel = fl_method_channel_new(
-      fl_engine_get_binary_messenger(engine),
-      "com.openlyst.doudou/window_controls",
-      FL_METHOD_CODEC_CODEC(fl_standard_method_codec_new()));
-
-  fl_method_channel_set_method_call_handler(channel, window_control_method_call,
-                                            g_object_ref(view), g_object_unref);
-}
-
 static void window_control_method_call(FlMethodChannel* channel,
                                        FlMethodCall* method_call,
                                        gpointer user_data) {
@@ -82,6 +46,42 @@ static void window_control_method_call(FlMethodChannel* channel,
   }
 
   fl_method_call_respond(method_call, response, nullptr);
+}
+
+static void setup_window_control_channel(FlView* view) {
+  FlEngine* engine = fl_view_get_engine(view);
+  g_autoptr(FlMethodChannel) channel = fl_method_channel_new(
+      fl_engine_get_binary_messenger(engine),
+      "com.openlyst.doudou/window_controls",
+      FL_METHOD_CODEC(fl_standard_method_codec_new()));
+
+  fl_method_channel_set_method_call_handler(channel, window_control_method_call,
+                                            g_object_ref(view), g_object_unref);
+}
+
+// Implements GApplication::activate.
+static void my_application_activate(GApplication* application) {
+  MyApplication* self = MY_APPLICATION(application);
+  GtkWindow* window =
+      GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+
+  gtk_window_set_title(window, "doudou");
+  gtk_window_set_decorated(window, FALSE);
+  gtk_window_set_default_size(window, 1280, 720);
+  gtk_widget_show(GTK_WIDGET(window));
+
+  g_autoptr(FlDartProject) project = fl_dart_project_new();
+  fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
+
+  FlView* view = fl_view_new(project);
+  gtk_widget_show(GTK_WIDGET(view));
+  gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
+
+  fl_register_plugins(FL_PLUGIN_REGISTRY(view));
+
+  gtk_widget_grab_focus(GTK_WIDGET(view));
+
+  setup_window_control_channel(view);
 }
 
 // Implements GApplication::local_command_line.
